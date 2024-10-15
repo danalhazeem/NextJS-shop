9dd2a60f070a7ce2fffe156b2b3d22ea51960c94
danalhazeem <92695924+danalhazeem@users.noreply.github.com>
2024-10-15 14:28:26 +0300

    hi

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,43 @@
 import Image from "next/image";
-import cookieShop from "./cookieShop.jpeg";
+import cookieShop from "./pics/cookieShop.jpeg";
+//import cookie from "./pics/cookie.jpeg";
+//import coffee from "./pics/coffee.jpeg";
+import products from "./products.js";
 
 export default function Home() {
+  const product = products.map((product) => (
+    <div key={product.id} className="product-item">
+      <Image src={product.image} alt={product.name} />
+      <p>{product.name}</p>
+      <p>Price: {product.price}</p>
+    </div>
+  ));
+
   return (
     <div className="grid items-center justify-items-center min ">
       <h1>Cookies and Beyond</h1>
       <h1>Where cookie maniacs gather</h1>
+
       <Image src={cookieShop} width={500} height={500} />
+      <h1>Our Products</h1>
+      <div className="flex justfy-center items-center gap-20 ">
+        {/* <div>
+          <Image src={cookie} width={200} height={200} />
+          <p>Chocolate Cookies</p>
+          <p>2.500</p>
+        </div>
+        <div>
+          <Image src={coffee} width={200} height={200} />
+          <p>Coffee </p>
+          <p>1.500</p>
+        </div>
+      */}
+      </div>
+      <div>
+        <div className="product-list flex justfy-center items-center">
+          {product}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/app/products.js
@@ -0,0 +1,26 @@
+import Cookie from "./pics/cookie.jpeg";
+import Coffee from "./pics/coffee.jpeg";
+import HoneyCake from "./pics/HoneyCake.jpeg";
+
+const products = [
+  {
+    id: 1,
+    name: "cookie",
+    price: 2.5,
+    image: Cookie,
+  },
+  {
+    id: 2,
+    name: "coffee",
+    price: 1.5,
+    image: Coffee,
+  },
+  ,
+  {
+    id: 3,
+    name: "Honey Cake",
+    price: 1.5,
+    image: HoneyCake,
+  },
+];
+export default products;
